609ce8d6da8f5f9a6c3c04a88a8294b09dd37b05
Babita1947 <130829271+Babita1947@users.noreply.github.com>
2023-11-27 19:41:32 +0530

    added Chapter_1 folder

---
+++ Chapter_1/DataTypes/string.js
@@ -0,0 +1,7 @@
+let str="abcd";
+str=str+"1234";
+console.log(str);
+
+let num="19";
+num=num+55;
+console.log(num);
---
+++ Chapter_1/Variables/variables.js
@@ -0,0 +1,46 @@
+// console.log("Welcome to Asansol Engineering College");  // log means print
+// radius=19;
+// age=20;
+// name='Babita Kumari';
+// //null means absent
+// x=null; 
+// y=undefined;
+// JavaScript is Dynamically Typed Language 
+
+//let,const & var
+// let name="tony stark";
+// let age=20;
+// let price=1000;
+// console.log(name);
+
+// let age=24;
+// age=19;
+// console.log(age);
+
+// const age=24;
+// age=19;
+
+
+// let a;
+// console.log(a);
+
+// const a=10;
+// console.log(a);
+
+let age=23;
+let price=200.5;
+let fullName='tony stark';
+isFollow=true;
+let x;
+let y=null;
+// console.log(typeof(age));
+// console.log(typeof(price));
+// console.log(typeof(fullName));
+// console.log(typeof(isFollow));
+console.log(x);
+console.log(typeof(x));
+console.log(y);
+console.log(typeof(y));
+
+
+
