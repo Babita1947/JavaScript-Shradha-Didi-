d72f785db9708bb1652cb0c1ec20187e2f4b4e2f
Babita1947 <130829271+Babita1947@users.noreply.github.com>
2023-11-28 22:02:09 +0530

    added code

---
+++ Chapter_2/script.js
@@ -0,0 +1,168 @@
+//This code print Hello World!
+/*This is a code that prints 
+  hello world on our console window */
+//console.log("Hello World!");
+
+// Arithmetic operators
+//let a=5;
+//let b=2;
+
+// console.log("a =", a, " & b = ",b);
+// console.log("a + b = ",a+b);
+// console.log("a - b = ",a-b);
+// console.log("a * b = ",a*b);
+// console.log("a / b = ",a/b);
+// Modulus operator
+//console.log("a % b = ",a%b);
+// Exponentiation operator
+//console.log("a ** b = ",a**b);
+
+// Unary Operator
+// let a=5;
+// let b=2;
+
+// console.log("a =", a, " & b = ",b);
+// a++;
+// b--;
+// console.log("a = ",a, "& b = ",b);
+// console.log("a = ",a++);
+// console.log("a = ",++a);
+// console.log("b = ",b--);
+// console.log("b = ",--b);
+
+// Assignment Operator
+// let a=5;
+// let b=2;
+// let c=3;
+// let d=4;
+// let e=1;
+
+// console.log("a = ",a,", b= ",b,", c = ",c,", d = ",d,", e = ",e);
+// a += 4; //a = a + 4
+// console.log("a += 4 => a = ",a);
+// b -= 1; //b = b - 1
+// console.log("b -= 1 => b = ",b);
+// c *= 2; //c = c * 2;
+// console.log("c *= 2 => c = ",c);
+// d %= 2; //d = d % 2;
+// console.log("d %= 2 => d = ",d);
+// e **= 2; //e = e ** 2;
+// console.log("e **= 2 => e = ",e);
+
+//Comparison Operator
+// let a=5;
+// let b=2;
+// let c=5;
+// console.log("5 == 2 ",a == b);//false
+// console.log("5 != 2 ",a != b);//true
+// console.log("5 == 5 ",a == c);//true
+// console.log("\n");
+// let x=5; //number
+// let y="5"; //string -> number
+
+// console.log("a = ",a," b = ",b," x = ",x," y = ",y);
+// console.log("x == y ",x == y);//true
+// console.log("x === y ",x === y);//false
+// console.log("x !== y ",x !== y);//true
+
+// console.log("x > y ",x>y);
+// console.log("x >= y ",x>=y);
+// console.log("a < b ",a<b);
+// console.log("a <= b ",a<=b);
+
+// Logical Operator
+// let a=6;
+// let b=5;
+
+
+// console.log("cond1 && cond2 = ",a>b && a===b);
+// console.log("cond1 || condn = ",a<b || a===b);
+// console.log("!(6<5) = ",!(a<b));
+
+
+// Conditional Statements
+// IF STATEMENT
+//let age=19;
+
+// if(age>18){
+//   console.log("You can vote");
+// }
+
+// if(age<18){
+//   console.log("You CANNOT vote");
+// }
+// let mode="dark";
+// let color;
+
+// if(mode === "dark"){
+//   color="black";
+// }
+
+// if(mode === "light"){
+//   color="white";
+// }
+
+// console.log(color);
+
+// IF-ELSE STATEMENT(Example)
+// let mode="dark";
+// let color;
+
+// if(mode === "dark"){
+//   color="black";
+// }
+
+// else{
+//   color="white";
+// }
+
+// console.log(color);
+
+//(Example)
+// let num=19;
+
+// if(num%2 === 0)
+// {
+//   console.log("Even");
+// }
+// else{
+//   console.log("Odd");
+// }
+
+//syntax -> rules
+
+//Example
+// let age=20;
+
+// if(age<18){
+//   console.log("Junior");
+// }
+// else if(age>60){
+//   console.log("Senior");
+// }
+// else {
+//   console.log("middle");
+// }
+
+// let mode = "dark";
+// let color;
+
+// if(mode === "dark"){
+//   color = "black";
+// }
+// else if(mode === "blue"){
+//   color = "blue";
+// }
+// else if(mode === "pink"){
+//   color = "pink";
+// }
+// else{
+//   color = "white";
+// }
+
+// console.log(color);
+
+//TERNARY OPERATOR
+let age = 19;
+let result = age >= 18 ? "adult" : "not adult"; //COMPACT IF-ELSE 
+console.log(result);
--- DataTypes/string.js
+++
@@ -1,7 +0,0 @@
-let str="abcd";
-str=str+"1234";
-console.log(str);
-
-let num="19";
-num=num+55;
-console.log(num);
